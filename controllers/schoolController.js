e0d0734ea0f34df0040891d703f7a34b42f1f809
aisarthak <aisarthak2@gmail.com>
2024-11-27 12:23:42 +0530

    add logging for request body in addSchool function

--- controllers/schoolController.js
+++ controllers/schoolController.js
@@ -10,6 +10,7 @@ const schoolSchema = Joi.object({
 
 export const addSchool = async (req, res) => {
   try {
+    console.log(req.body);
     const { error } = schoolSchema.validate(req.body);
     if (error) {
       return res.status(400).json({ error: error.details[0].message });
